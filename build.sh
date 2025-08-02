dcbdb43f32fcdce6ad66fc5d7d85e7d4fda45e80
Jason Short <jason@sheersky.com>
2025-08-02 10:10:10 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -e
 go build -ldflags "-s -w"
 echo "Before compressing binary:"
 ls -l clexpg
-upx clexpg
-echo "After compressing binary:"
-ls -l clexpg
+# UPX no longer supported on MacOS:
+# upx: clexpg: CantPackException: macOS is currently not supported (try --force-macos)
+#upx clexpg
+#echo "After compressing binary:"
+#ls -l clexpg
